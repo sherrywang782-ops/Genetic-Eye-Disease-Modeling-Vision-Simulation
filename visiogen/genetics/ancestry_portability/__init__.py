7eb9697088777af5ceb404a6868171d9fcc4c25e
14386 <sherrywang782@gmail.com>
2026-03-22 16:46:53 -0400

    fixed the import statement for the ancestry_portability module in the notebooks

--- visiogen/genetics/ancestry_portability/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Ancestry portability evaluation for PRS models.
-
-This package contains tools to quantify how PRS performance degrades as a
-function of genetic distance from the training population.
-"""
